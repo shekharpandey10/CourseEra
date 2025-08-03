6dc457785b8be7d42d75172ad162ecbcae3777ee
Chandra shekhar pandey <158845488+shekharpandey10@users.noreply.github.com>
2025-08-03 17:32:20 +0530

    server client architecture added

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express=require('express')
 const Router=express.Router
-const {userRouter}=require("./router/user")
+const {userRouter}=require("../router/user")
     
-const {courseRouter}=require('./router/course')
-const {adminRouter}=require('./router/admin')
+const {courseRouter}=require('../router/course')
+const {adminRouter}=require('../router/admin')
 const app=express()
 
 app.use('/user',userRouter)
